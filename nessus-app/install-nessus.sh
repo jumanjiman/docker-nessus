9b4ddad6e9ba24357b8d802079c8ee8605de4fcd
Tim Kropp <timkropp77@gmail.com>
2018-03-24 15:58:22 -0400

    add back Nessus local rpm (#44)
    * add back Nessus local rpm

--- nessus-app/install-nessus.sh
+++ nessus-app/install-nessus.sh
@@ -3,18 +3,8 @@ set -eEux
 set -o pipefail
 
 # Import Tenable's GPG key.
-rpm --import https://static.tenable.com/marketing/RPM-GPG-KEY-Tenable
+rpm --import tenable-2048.gpg
 
-readonly NESSUS_VERSION="${NESSUS_VERSION:-7.0.2}"
-readonly RPM_NAME="Nessus-${NESSUS_VERSION}-es7.x86_64.rpm"
-readonly BASE_URL="https://tenable-downloads-production.s3.amazonaws.com/uploads/download/file"
-
-# Find the ID.
-DOWNLOAD_ID="$(
-  curl -ssl -o - "https://www.tenable.com/downloads/nessus" |
-  sed -n -e "s/.*data-download-id=\"\\([0-9]*\\)\".*data-file-name=\"${RPM_NAME}\".*/\\1/p"
-)"
-readonly DOWNLOAD_ID
 
 # Install the package.
-rpm -Uvh "${BASE_URL}/${DOWNLOAD_ID}/${RPM_NAME}"
+rpm -Uvh Nessus-7.0.3-es7.x86_64.rpm
